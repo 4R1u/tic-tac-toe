b04d3e1f6acb7535ba82e5a4515c91a2a3dc71fc
4R1u <62248461+4R1u@users.noreply.github.com>
2025-07-18 13:06:53 +0400

    feat: add winner function to gameBoard

--- script.js
+++ script.js
@@ -21,8 +21,27 @@ const gameBoard = (function () {
         if (placeMark(i)) switchCurrentPlayer();
     };
 
-    const getBoard = () =>
-        [board[0], board[1], board[2], board[3], board[4], board[5], board[6], board[7], board[8]];
+    const getBoard = () => [board[0], board[1], board[2], board[3], board[4], board[5], board[6], board[7], board[8]];
 
-    return { getCurrentPlayer, playRound, getBoard };
+    const getWinner = () => {
+        const winningCombos = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],
+            [0, 3, 6], [1, 4, 7], [2, 5, 8],
+            [0, 4, 8], [2, 4, 6]
+        ];
+
+        for (let i = 0; i < winningCombos.length; ++i) {
+            if (board[winningCombos[i][0]] != "" && board[winningCombos[i][0]] == board[winningCombos[i][1]] &&
+                board[winningCombos[i][1]] == board[winningCombos[i][2]])
+                return board[winningCombos[i][0]];
+        }
+
+        return null;
+    };
+
+    const reset = () => {
+        for (let i = 0; i < 9; ++i) board[i] = "";
+    };
+
+    return { getCurrentPlayer, playRound, getBoard, getWinner, reset };
 })();
